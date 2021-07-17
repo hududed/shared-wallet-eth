92be75eaddf81a923a4020998596d7420c11b508
hud <hudwahab@gmail.com>
2021-07-17 16:57:06 -0600

    Allowance.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,32 +1,6 @@
 pragma solidity 0.8.1;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
-contract Allowance is Ownable {
-    
-    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
-    
-    function isOwner() internal view returns(bool) {
-        return owner() == msg.sender;
-    }
-    
-    mapping(address => uint) public allowance;
-    
-    function setAllowance(address _who, uint _amount) public onlyOwner {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount );
-        allowance[_who] = _amount;
-    }
-    
-    modifier ownerOrAllowed(uint _amount) {
-        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed!");
-        _;
-    }
-
-    function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount){
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount );
-        allowance[_who] -= _amount;
-    }
-}
+import "./Allowance.sol";
 
 contract SharedWallet is Allowance {
     
@@ -41,6 +15,10 @@ contract SharedWallet is Allowance {
         }
         emit MoneySent(_to, _amount);
         _to.transfer(_amount);
+    }
+    
+    function renounceOwnership() public override onlyOwner {
+        revert("can't renounceOwnership here"); //not possible with this smart contract
     }
 
     receive() external payable {
